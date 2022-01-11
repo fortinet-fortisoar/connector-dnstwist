5fc18cbf62965e0c487299324e63feeceb123141
Şemseddin AKSOY <semsaksoy@gmail.com>
2022-01-11 08:51:13 +0300

    logger

--- connector.py
+++ connector.py
@@ -3,7 +3,7 @@ from connectors.core.connector import Connector, ConnectorError, get_logger
 from .operations import *
 from .operations import _check_health
 
-logger = get_logger('screenshot')
+logger = get_logger('dnstwist')
 
 class DnsTwist(Connector):
     def execute(self, config, operation_name, params, **kwargs):
