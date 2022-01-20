27909018d03923196e24cec9760b91f7f757e868
Şemseddin AKSOY <semsaksoy@gmail.com>
2022-01-20 09:54:50 +0300

    map for actions,import fix

--- dnstwist/connector.py
+++ dnstwist/connector.py
@@ -1,21 +1,19 @@
 from connectors.core.connector import Connector, ConnectorError, get_logger
-
-from .operations import *
-from .operations import _check_health
+from .operations import _check_health, search
 
 logger = get_logger('dnstwist')
 
 
 class DnsTwist(Connector):
+
     def execute(self, config, operation_name, params, **kwargs):
         logger.info("operation_name: {0}".format(operation_name))
-        result = None
-        if operation_name == 'search':
-            result = search(config, params)
-        return result
 
-
-
+        operations = {
+            'search': search,
+        }
+        action = operations.get(operation_name)
+        return action(config, params)
 
     def check_health(self, config):
         try:
